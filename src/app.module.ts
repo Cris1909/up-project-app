69a6d33b71999e264041b52e1fe5bd911e1c80ee
Cris <115098799+Cris1909@users.noreply.github.com>
2023-08-17 23:58:01 -0500

    configurando el proyecto con env y mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }),
+
+    MongooseModule.forRoot(process.env.MONGO_DB),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  environment: string;
+
+  constructor(
+    private readonly configService: ConfigService,
+  ) {
+    this.environment = configService.get<string>('environment');
+  }
+
+  getHello() {
+    return {
+      message: 'Welcome to my api',
+      environment: this.environment,
+    };
   }
 }
